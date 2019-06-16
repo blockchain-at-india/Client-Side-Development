d333ed44aea9fd858122f5d329d1a62feb83333c
Faraz Khan <farazrk001@gmail.com>
2019-06-16 20:03:48 +0530

    Add carousel

---
+++ script.js
@@ -0,0 +1,23 @@
+$(document).ready(() => {
+  $('.toggle-button').click(() => {
+    $('.navlinks').toggleClass('show')
+  })
+
+  new Siema({
+    selector: '.cards',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: {
+      0: 1,
+      1190: 3
+    },
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {}
+  })
+})
