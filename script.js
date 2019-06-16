6554c1674d7ecfaf108f442c4efcb9f2f4ed84df
Faraz Khan <farazrk001@gmail.com>
2019-06-16 22:15:17 +0530

    Add carousel for testimonials

--- script.js
+++ script.js
@@ -38,4 +38,27 @@ $(document).ready(() => {
     onInit: () => {},
     onChange: () => {}
   })
+
+  var test = new Siema({
+    selector: '.test',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {}
+  })
+
+  $('.left-arrow').click(() => {
+    test.prev()
+  })
+
+  $('.right-arrow').click(() => {
+    test.next()
+  })
 })
