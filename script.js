48d63fd99f2998713ad508680ceb385030fa89f8
Faraz Khan <farazrk001@gmail.com>
2019-06-18 18:17:37 +0530

    V3

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
+let cards, profileCards, test
+
 document.addEventListener('DOMContentLoaded', () => {
+  const leftArrow = document.querySelectorAll('.left-arrow')
+  const rightArrow = document.querySelectorAll('.right-arrow')
   const toggleButton = document.querySelector('.toggle-button')
   const navLinks = document.querySelector('.navlinks')
   toggleButton.addEventListener('click', () => {
     navLinks.classList.toggle('show')
   })
 
-  const cards = new Siema({
+
+  cards = new Siema({
     selector: '.cards',
     duration: 200,
     easing: 'ease-out',
@@ -23,7 +28,21 @@ document.addEventListener('DOMContentLoaded', () => {
     onChange: () => {}
   })
 
-  const profileCards = new Siema({
+  test = new Siema({
+    selector: '.test',
+    duration: 200,
+    easing: 'ease-out',
+    perPage: 1,
+    startIndex: 0,
+    draggable: true,
+    multipleDrag: true,
+    threshold: 20,
+    loop: true,
+    rtl: false,
+    onInit: () => {},
+    onChange: () => {}
+  })
+  profileCards = new Siema({
     selector: '.profile-cards',
     duration: 200,
     easing: 'ease-out',
@@ -41,21 +60,29 @@ document.addEventListener('DOMContentLoaded', () => {
     onChange: () => {}
   })
 
-  const test = new Siema({
-    selector: '.test',
-    duration: 200,
-    easing: 'ease-out',
-    perPage: 1,
-    startIndex: 0,
-    draggable: true,
-    multipleDrag: true,
-    threshold: 20,
-    loop: true,
-    rtl: false,
-    onInit: () => {},
-    onChange: () => {}
-  })
+  function testLeft() {
+    test.prev()
+  }
+  function testRight() {
+    test.next()
+  }
+
+  function profileLeft() {
+    profileCards.prev()
+  }
+  function profileRight() {
+    profileCards.next()
+  }
+
+  leftArrow[0].addEventListener('click', testLeft, false)
+  rightArrow[0].addEventListener('click', testRight, false)
+
+  leftArrow[1].addEventListener('click', profileLeft, false)
+  rightArrow[1].addEventListener('click', profileRight, false)
 })
+
+
+
 
 //   $('.left-arrow').click(() => {
 //     test.prev()
